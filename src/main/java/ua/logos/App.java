a4673ab477c81ddf944076b908ab798352c2bd0d
Oleg Sokolyk <olegs.logos@gmail.com>
2018-02-14 02:46:00 +0200

    Completed database

--- src/main/java/ua/logos/App.java
+++ src/main/java/ua/logos/App.java
@@ -1,8 +1,6 @@
 package ua.logos;
 
 import java.math.BigDecimal;
-import java.util.Date;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -25,42 +23,65 @@ public class App
         
         
         em.getTransaction().begin();
-        for (int i = 1; i <= 20; i++) {
-        	CarModel model = new CarModel("Model #" + i);
-        	em.persist(model);
-		}
         
-//        for (int i = 1; i <= 50; i++) {
-//        	CarModel model = em.createQuery("SELECT m FROM CarModel m WHERE m.id = :id", CarModel.class)
-//        			.setParameter("id", new Long(i))
-//        			.getSingleResult();	
-//        	CarMake make = new CarMake("Make " + i, new Date(2, 2, 1998));
-//        	make.setCarModel(model);
+//        for (int i = 1; i <= 20; i++) {
+//        	CarSeller carSeller = new CarSeller("name-" + i, "last name-" + i, 25, 380984786700L + i);
+//        	em.persist(carSeller);   	
+//		}    
+        
+//        for (double i = 1.6; i <= 5.4; i += 0.2) {
+//        	CarEngineCapacity engineCapacity = new CarEngineCapacity(new BigDecimal(i));
+//        	em.persist(engineCapacity);
+//		}
+        
+//        for (int i = 1; i <= 20; i++) {
+//        	CarColor color = new CarColor("Color #" + i);
+//        	em.persist(color);
+//        }
+//        
+//        for (int i = 1; i <= 20; i++) {
+//        	CarFuelType fuelType = new CarFuelType("Fuel #" + i);
+//        	em.persist(fuelType);
 //        }
         
-        for (int i = 1; i < 20; i++) {
-        	CarSeller carSeller = new CarSeller("name-" + i, "last name-" + i, 25, 380984786700L + i);
-        	em.persist(carSeller);   	
-		}
+//        for (int i = 1; i <= 20; i++) {
+//        	        	
+//        	CarColor color = em.createQuery("SELECT c FROM CarColor c WHERE c.id = :id", CarColor.class)
+//        			.setParameter("id", new Long(i)).getSingleResult();
+//        	
+//        	CarFuelType fuelType = em.createQuery("SELECT f FROM CarFuelType f WHERE f.id = :id", CarFuelType.class)
+//        			.setParameter("id", new Long(i)).getSingleResult();
+//        	
+//        	CarEngineCapacity engineCapacity = em.createQuery("SELECT cec FROM CarEngineCapacity cec WHERE cec.id = :id", CarEngineCapacity.class)
+//        			.setParameter("id", new Long(i)).getSingleResult();
+//        	 	
+//        	CarModel model = new CarModel("Model #" + i);
+//        	model.setCarColor(color);
+//        	model.setEngineCapacity(engineCapacity);
+//        	model.setFuelType(fuelType);
+//        	em.persist(model);
+//		}
         
-        for (double i = 1.6; i <= 2.8; i += 0.2) {
-        	CarEngineCapacity engineCapacity = new CarEngineCapacity(new BigDecimal(i));
-        	em.persist(engineCapacity);
-		}
+//      for (int i = 1; i <= 20; i++) {
+//    	CarModel model = em.createQuery("SELECT m FROM CarModel m WHERE m.id = :id", CarModel.class)
+//    			.setParameter("id", new Long(i)).getSingleResult();	
+//    	CarMake make = new CarMake("Make " + i, 1950 + i);
+//    	make.setCarModel(model);
+//    	em.persist(make);
+//      }
+//      
+       double price = 100000.0;
         
-        for (int i = 1; i < 10; i++) {
-        	CarColor color = new CarColor("Color #" + i);
-        	em.persist(color);
-        }
-        
-        for (int i = 1; i < 10; i++) {
-        	CarFuelType fuelType = new CarFuelType("Fuel #" + i);
-        	em.persist(fuelType);
-        }
-        
-        for (double i = 100000.0; i <= 200000.0; i += 5000.0) {
-        	Car car = new Car(new BigDecimal(i));
+       for (int i = 1; i <= 20; i++) {
+        	CarMake make = em.createQuery("SELECT cm FROM CarMake cm WHERE cm.id = :id", CarMake.class)
+        			.setParameter("id", new Long(i)).getSingleResult();
+        	CarSeller seller = em.createQuery("SELECT s FROM CarSeller s WHERE s.id = :id", CarSeller.class)
+        			.setParameter("id", new Long(i)).getSingleResult();
+        	Car car = new Car(new BigDecimal(price));
+        	car.setCarMake(make);
+        	car.setCarSeller(seller);
         	em.persist(car);
+        	price+=5000;
 		}
         
         em.getTransaction().commit();
--- src/main/java/ua/logos/entity/CarEngineCapacity.java
+++ src/main/java/ua/logos/entity/CarEngineCapacity.java
@@ -41,8 +41,5 @@ public class CarEngineCapacity extends BaseEntity{
 		this.carModel = carModel;
 	}
 
-
-	
-	
 	
 }
